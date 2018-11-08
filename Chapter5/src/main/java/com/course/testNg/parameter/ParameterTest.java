c51df497ea418459ff85fd6abdfa124219849cba
chengjingjing1992 <2629704685@qq.com>
2018-11-08 17:43:58 +0800

    依赖测试

---
+++ Chapter5/src/main/java/com/course/testNg/parameter/ParameterTest.java
@@ -0,0 +1,13 @@
+package com.course.testNg.parameter;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParameterTest {
+
+    @Test
+    @Parameters({"name","age"})//注意这里怎么写
+    public void parameterTest(String name,int age){
+        System.out.println("name="+name +"  ，  age="+age);
+    }
+}
