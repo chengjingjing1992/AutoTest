8f35b79c5f4989ef44ab23d37c6263d45338ac33
chengjingjing1992 <2629704685@qq.com>
2018-11-09 11:44:02 +0800

    依赖测试

--- Chapter5/src/main/java/com/course/testNg/parameter/DataProvider.java
+++ Chapter5/src/main/java/com/course/testNg/parameter/DataProvider.java
@@ -2,6 +2,8 @@ package com.course.testNg.parameter;
 
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;//！！！注意导入的是reflect 的包
+
 public class DataProvider {
     @Test(dataProvider = "data")
     public void testDataProvider(String name , int age){
@@ -19,5 +21,45 @@ public class DataProvider {
         };
         return o;
     }
+
+    /**
+     * 根据不同的方法  传不同的参数
+     * @param name
+     * @param age
+     */
+    @Test(dataProvider = "sss")
+    public  void testA(String name , int age){
+        System.out.println("testA"+"name="+name +";  age="+age);
+    }
+    @Test(dataProvider = "sss")
+    public  void testB(String name , int age){
+        System.out.println("testA"+"name="+name +";  age="+age);
+    }
+    @org.testng.annotations.DataProvider(name = "sss")
+    public Object [][] providerData1(Method method){  //！！！Method method 通过这个获取不同的 方法名
+        Object[][] o;
+        if (method.getName().equals("testA")){
+            o=new Object[][]{
+                    {"zhangSan",25},//注意上面的 是 age 是int 类型,那么 这里的25 是不带双引号的
+                    {"Lisi",35},
+                    {"wangWu",45}
+            };
+            return o;
+        };
+        if (method.getName().equals("testB")){
+                o=new Object[][]{
+                        {"wukong",1000},//注意上面的 是 age 是int 类型,那么 这里的25 是不带双引号的
+                        {"nezha",500},
+                        {"erlangshne",600}
+                };
+                return o;
+
+        }
+        return null;
+    }
+
+
+
+
 
 }
